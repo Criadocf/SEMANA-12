86f8fb1b4623a8704d0dddc0be4e56b3559086b1
KleberJunior <kleberjrmmxvi@gmail.com>
2022-05-30 21:21:59 -0300

    TENHO Q TERMINAR DEPOIS, POIS VENCEU HOJE

---
+++ etapa_1-desenhando_estrelas.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+color ("whitesmoke")
+bgcolor("midnightblue")
+
+pendown()
+begin_fill()
+
+for side in range(5):
+    left(144)
+    forward(50)
+
+end_fill()
+penup() #PARA DE DESENHAR
+
+forward(100)
+done()
